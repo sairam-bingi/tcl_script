59260a7e7f96fc6db10ecc4730c7e89ab87b8f41
Sairam Bingi <43058287+sairam-bingi@users.noreply.github.com>
2021-06-07 19:42:22 +0530

    File handling basic level
    The committed file "file_handle.tcl" contains a basic file writing code.

---
+++ file_handle.tcl
@@ -0,0 +1,26 @@
+# ##############################################
+#                                              #
+# Designer: Sairam Bingi                         #
+# Topic: File handling in Tcl/Tk               #
+# Language: Tcl/Tk                             #
+# Contents:                                    #
+#    1. Simple file writing example            #
+#                                              #
+# Note: Suggestion are welcomed.               #
+#                                              #
+# ##############################################
+
+proc simple_txt_file_wr { file_size } {
+   puts "This code demonstrates basic file writing"
+   set filename "output_file.txt"
+   puts "Results will be stores in 'output_file.txt'"
+   set fileid [open $filename "w"]
+   set i 0
+   while { $i < $file_size } {
+      incr i
+      set file_data "The current line is $i"
+      puts $fileid $file_data
+   }
+   close $fileid
+   puts "File writing completed"
+}
